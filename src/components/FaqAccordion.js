ff90cb93b362436adc146f72423ef19161021530
Jordan <rectorjordan94@gmail.com>
2024-05-02 14:47:49 -0700

    fixes logic for changing button between plus and minus svg upon clicking

--- src/components/FaqAccordion.js
+++ src/components/FaqAccordion.js
@@ -6,20 +6,25 @@ const FaqAccordion = (props) => {
     // will probably need to use 'useEffect' to change the state of each question's answer from invisible to visible
 
     const [showAnswer, setShowAnswer] = useState(false)
+    const [buttonSvg, setButtonSvg] = useState(<></>)
     // const [questionClicked, setQuestionClicked] = useState('')
 
     // console.log(props.faqs)
 
     const handleClick = (e) => {
         setShowAnswer(prev => !prev)
-        // console.log(e.target.dataset.key)
+        
         const answerToShow = document.querySelector(`#p-${e.target.dataset.key}`)
+        const buttonToChange = document.querySelector(`#button-${e.target.dataset.key}`)
         // console.log(answerToShow)
         if (answerToShow.style.display === 'none') {
             answerToShow.style.display = 'block'
+            buttonToChange.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#301534" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"/></svg>`
         } else {
             answerToShow.style.display = 'none'
+            buttonToChange.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/></svg>`
         }
+        
     }
 
     // if the text or the button is clicked, set something to true? when that something is set to true, show the <p> below the question that holds the answer to the question
@@ -30,7 +35,7 @@ const FaqAccordion = (props) => {
             <div className="faq-inner" key={i}>
                 <div className="question-container">
                     <button className="question" onClick={handleClick} data-key={i}>{faq.question}</button>
-                    <button className="show-hide-button" onClick={handleClick} value={faq.answer} data-key={i}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/></svg></button>
+                    <button className="show-hide-button" id={`button-${i}`} onClick={handleClick} value={faq.answer} data-key={i}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/></svg></button>
                 </div>
                 <p className="hidden answer" id={`p-${i}`}>{faq.answer}</p>
                 <hr id={`hr-${i}`} />
