001695dbeca30ee7d28458f5a68ecaf823729e37
Jordan <rectorjordan94@gmail.com>
2024-05-02 12:26:17 -0700

    adds some of the svgs to the html in appropriate locations, adds hrs after each p tag

--- src/components/FaqAccordion.js
+++ src/components/FaqAccordion.js
@@ -30,15 +30,17 @@ const FaqAccordion = (props) => {
             <div className="faq-inner" key={i}>
                 <div className="question-container">
                     <button className="question" onClick={handleClick} data-key={i}>{faq.question}</button>
-                    <button className="show-hide-button" onClick={handleClick} value={faq.answer} data-key={i}>+</button>
+                    <button className="show-hide-button" onClick={handleClick} value={faq.answer} data-key={i}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/></svg></button>
                 </div>
                 <p className="hidden answer" id={`p-${i}`}>{faq.answer}</p>
+                <hr />
             </div>
         )
     })
 
     return (
         <div className="faq-outer">
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="41" fill="none" viewBox="0 0 40 41"><path fill="#AD28EB" d="M37.5 20.5a2.467 2.467 0 0 1-1.64 2.344l-9.913 3.604-3.603 9.911a2.5 2.5 0 0 1-4.688 0l-3.604-9.922-9.911-3.593a2.5 2.5 0 0 1 0-4.688l9.921-3.604 3.594-9.911a2.5 2.5 0 0 1 4.688 0l3.604 9.921 9.911 3.594A2.467 2.467 0 0 1 37.5 20.5Z"/></svg>
             <h1 className="title">FAQs</h1>
             {faqs}
         </div>
